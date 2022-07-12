de112091d7b4053b28b190e3c6664e3302615a02
Jagteshver Singh Sahni <85994817+JS-CodeGamer@users.noreply.github.com>
2022-07-12 21:31:54 +0530

    refactor: Refactor master.py
    - Refactored master.py to include all raspberry-pi code in a function

--- master.py
+++ master.py
@@ -5,6 +5,7 @@ from log_handler import open_log_file, close_log_file, print_log_msg
 
 log_file_name = "fire_proj_main"
 old_stdout, log_file = open_log_file(log_file_name)
+
 set_env(required = {"twilio", "gps",
                 "picamera", "tensorflow",
                 "numpy", "matplotlib",
@@ -18,8 +19,9 @@ set_env(required = {"twilio", "gps",
 
 
 
+## Uncomment these imports before running on Rasberry-Pi
 # import picamera
-from time import time, sleep, gmtime, strftime
+from time import time, sleep
 from keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
@@ -30,6 +32,7 @@ from model_maker import make_model
 
 
 
+## Function for detecting fire in image
 def parse_image(image_for_testing, model):
     print_log_msg("Computing result from image...")
     test_image=image.load_img(image_for_testing,target_size=(224,224))
@@ -42,34 +45,44 @@ def parse_image(image_for_testing, model):
 
 
 
-if not os.path.exists(os.path.join(os.getcwd(), 'Smoke_and_Fire_Classifier.h5')):
+# This function is for running the entire project on Rasberry-Pi
+def run_on_pi():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (256, 256)
+        camera.framerate = 80
+        camera.start_preview()
+        sleep(1)
+
+        start = time()
+        for filename in camera.capture_continuous('./testFolder/image{timestamp:%H:%M:%S.%f}.jpg'):
+            prediction = parse_image(filename,my_model)
+            finish = time()
+            print_log_msg('Captured %s at %.2ffps' % (filename, 1 / (finish - start)))
+            print_log_msg("Sending email and message...")
+            send_email_message(lat_lon=getPositionData(),
+                            receivers=get_recievers(),
+                            warning_level=prediction)
+            print_log_msg("Email and message sent.")
+            sleep(0.1)
+            start = time()
+
+
+
+## Check if model exists, if not create it
+if not os.path.exists(os.path.join(os.getcwd(), 'Image_Classifier.h5')):
     print_log_msg("Making Model...")
     make_model()
     print_log_msg("Model Completed.")
 
 print_log_msg("Loading Model...")
-my_model = load_model("./Smoke_and_Fire_Classifier.h5")
+my_model = load_model("./Image_Classifier.h5")
 print_log_msg("Model Loaded.")
 
-# with picamera.PiCamera() as camera:
-#     camera.resolution = (256, 256)
-#     camera.framerate = 80
-#     camera.start_preview()
-#     sleep(1)
+## Uncomment in Rasberry-Pi, does not run in other os's
+# run_on_pi()
 
-#     start = time()
-#     for filename in camera.capture_continuous('./testFolder/image{timestamp:%H:%M:%S.%f}.jpg'):
-#         prediction = parse_image(filename,my_model)
-#         finish = time()
-#         print_log_msg('Captured %s at %.2ffps' % (filename, 1 / (finish - start)))
-#         print_log_msg("Sending email and message...")
-#         send_email_message(lat_lon=getPositionData(),
-#                         receivers=get_recievers(),
-#                         warning_level=prediction)
-#         print_log_msg("Email and message sent.")
-#         sleep(0.1)
-#         start = time()
 
+## Test Code
 send_email_message(lat_lon= (45, 45),
                 receivers=get_recievers(),
                 warning_level=parse_image('fire.jpg',my_model))
--- model_maker.py
+++ model_maker.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+import tensorflow.keras as tk
 import numpy as np
 import cv2
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -8,36 +8,37 @@ def make_model():
     train=ImageDataGenerator(rescale=1/255)
     validation=ImageDataGenerator(rescale=1/255)
 
-    train_dataset=train.flow_from_directory('Dataset/Training/',target_size=(224,224),
-                                        class_mode='categorical',shuffle=True)
-    validation_dataset=validation.flow_from_directory('Dataset/validation/',target_size=(224,224)
-                                        ,class_mode='categorical',shuffle=True)
+    train_dataset=train.flow_from_directory('Model/Dataset/Training/',target_size=(224,224),
+                                            class_mode='categorical',shuffle=True)
+    validation_dataset=validation.flow_from_directory('model/Dataset/validation/',target_size=(224,224)
+                                                        ,class_mode='categorical',shuffle=True)
 
 
-    model=tf.keras.models.Sequential([tf.keras.layers.Conv2D(16,(3,3),activation='relu',input_shape=(224,224,3)),
-                                    tf.keras.layers.MaxPool2D(2,2),
-                                    tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=(224,224,3)),
-                                    tf.keras.layers.MaxPool2D(2,2),
-                                    tf.keras.layers.Conv2D(64,(3,3),activation='relu',input_shape=(224,224,3)),
-                                    tf.keras.layers.MaxPool2D(2,2),
-                                    tf.keras.layers.Flatten(),
-                                    tf.keras.layers.Dense(512,activation='relu'),
-                                    tf.keras.layers.Dense(3,activation='softmax')])
+    model=tk.models.Sequential([tk.layers.Conv2D(16,(3,3),activation='relu',input_shape=(224,224,3)),
+                                tk.layers.MaxPool2D(2,2),
+                                tk.layers.Conv2D(32,(3,3),activation='relu',input_shape=(224,224,3)),
+                                tk.layers.MaxPool2D(2,2),
+                                tk.layers.Conv2D(64,(3,3),activation='relu',input_shape=(224,224,3)),
+                                tk.layers.MaxPool2D(2,2),
+                                tk.layers.Flatten(),
+                                tk.layers.Dense(512,activation='relu'),
+                                tk.layers.Dense(3,activation='softmax')])
 
     model.compile(loss='categorical_crossentropy',
                     optimizer=Adam(lr=0.0001),
                     metrics=['acc'])
 
-    history= model.fit(train_dataset,
+    history=model.fit(train_dataset,
                         steps_per_epoch = 15,
                         epochs =100,
                         validation_data = validation_dataset,
-                        validation_steps = 15)
+                        validation_steps = 15,
+                        verbose=0)
 
     score = model.evaluate(validation_dataset)
     print('Test accuracy:', score[1])
 
-    model.save("Smoke_and_Fire_Classifier.h5")
+    model.save("Image_Classifier.h5")
 
 if __name__ == "__main__" :
     make_model()
